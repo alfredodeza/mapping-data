769ea8da00888e8ec759e56cb65a77b448482f84
Daniel Tschanz <datscha4478@gmail.com>
2024-01-26 16:18:30 +0100

    Adding 'index' as head csv-files

--- sample_data/generate_sql.py
+++ sample_data/generate_sql.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("wine-ratings-small.csv", index_col=0)
+df = pd.read_csv("sample_data/wine-ratings-small.csv", index_col=0) # chose subfolder
 sql_tmpl = """INSERT INTO ratings(name, rating, region) VALUES("%s", "%s", "%s");\n"""
 sql_file = open("populate.sql", "a")
 
 for _, row in df.iterrows():
     sql_file.write(sql_tmpl % (row['name'], row['rating'], row['region']))
-    #print(row)
+    print(row)
